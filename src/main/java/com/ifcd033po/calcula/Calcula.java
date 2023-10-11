3d640d9ac17631e9357b465c799117375e678e2c
aredondop <102090402+aredondop@users.noreply.github.com>
2023-10-11 15:03:47 +0200

    inicial

---
+++ src/main/java/com/ifcd033po/calcula/Calcula.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ifcd033po.calcula;
+
+/**
+ *
+ * @author Ángel Redondo
+ */
+
+public class Calcula {
+
+    public static void main(String[] args) {
+        
+        int miNumero = 5;
+        int miFactorial = calcularFactorial(miNumero);
+        
+        System.out.println("El factorial de "+ miNumero + " es " + miFactorial);
+        System.out.println("Los alumnos de este curso aprenden Programación en JAVA");
+    }
+    
+    public static int calcularFactorial( int numero){
+        int factorial = 1;
+        
+        for (int i = 1; i <= numero; i++) {
+            factorial *=i;
+        }
+        
+        return factorial;
+    }
+}
